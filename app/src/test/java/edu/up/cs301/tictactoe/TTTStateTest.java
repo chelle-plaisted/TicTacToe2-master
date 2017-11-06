79414ca3334cb77911922430bc0df32efb6b29e5
plaisted20 <plaisted20@up.edu>
2017-11-06 09:04:01 -0800

    Initial commit

---
+++ app/src/test/java/edu/up/cs301/tictactoe/TTTStateTest.java
@@ -0,0 +1,22 @@
+package edu.up.cs301.tictactoe;
+
+import org.junit.Test;
+
+import static junit.framework.Assert.assertTrue;
+
+/**
+ * Created by plaisted20 on 11/6/2017.
+ */
+
+public class TTTStateTest {
+    @Test
+    public void testConstr() {
+        TTTState state = new TTTState();
+        for (int row = 0; row <=2; row++) {
+            for(int col = 0; col <=2; col++) {
+                char ch = state.getPiece(row, col);
+                assertTrue("Initial state bad", ch == ' ');
+            }
+        }
+    }
+}
